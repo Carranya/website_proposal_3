16b135ff78180fc38a893874abe855cb04bc6ccc
Carranya <k.gong82@gmail.com>
2022-09-24 10:30:53 +0200

    create mainpart

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=egde">
-    <meta name="viewport" content="width=device-width, initial_scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website Proposal 3</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <div id="head"><p id="headTitle">Head</p></div>
+    <div id="main">
+        <img src="img/karin.jpg" id="idMainPic"></img>
+        <!--<div class="contentPage">Page</div>-->
+    </div>
+    <div id="foot">Foot</div>
 </body>
 </html>
